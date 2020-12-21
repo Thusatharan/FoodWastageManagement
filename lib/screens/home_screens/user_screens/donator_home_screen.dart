f82635008c2382928060a49688e21b74dc079e01
TThishanth <thishanth41@gmail.com>
2020-12-21 23:08:13 +0530

    Third Commit

--- lib/screens/home_screens/user_screens/donator_home_screen.dart
+++ lib/screens/home_screens/user_screens/donator_home_screen.dart
@@ -1,26 +1,97 @@
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:food_wastage_management/providers/auth_provider.dart';
 import 'package:food_wastage_management/screens/auth_screens/auth_screen.dart';
+import 'package:food_wastage_management/widgets/profile_screen_widget.dart';
 import 'package:provider/provider.dart';
 
-class DonatorHomeScreen extends StatelessWidget {
+class DonatorHomeScreen extends StatefulWidget {
   static const routeName = '/donator_home_screen';
-  
+
+  @override
+  _DonatorHomeScreenState createState() => _DonatorHomeScreenState();
+}
+
+class _DonatorHomeScreenState extends State<DonatorHomeScreen> {
+  User _currentUser = FirebaseAuth.instance.currentUser;
+  var _currentIndex = 0;
+
+  void _changePage(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  homeScreen() {}
+
+  requestScreen() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          child: Text('Donator Screen'),
-          onPressed: () {
-            Provider.of<Authentication>(context, listen: false)
-                .signOut()
-                .then((_) {
-              Navigator.of(context).pushReplacementNamed(AuthScreen.routeName);
-            });
-          },
-        ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Theme.of(context).primaryColor,
+        onPressed: () {},
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BubbleBottomBar(
+        hasNotch: true,
+        fabLocation: BubbleBottomBarFabLocation.end,
+        opacity: 0.2,
+        currentIndex: _currentIndex,
+        onTap: _changePage,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(15.0),
+        ),
+        elevation: 10.0,
+        items: <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(
+              Icons.dashboard,
+              color: Colors.black,
+            ),
+            activeIcon: Icon(
+              Icons.dashboard,
+              color: Theme.of(context).primaryColor,
+            ),
+            title: Text('Home'),
+          ),
+          BubbleBottomBarItem(
+            backgroundColor: Colors.deepPurple,
+            icon: Icon(
+              Icons.access_time,
+              color: Colors.black,
+            ),
+            activeIcon: Icon(
+              Icons.access_time,
+              color: Colors.deepPurple,
+            ),
+            title: Text('Requests'),
+          ),
+          BubbleBottomBarItem(
+            backgroundColor: Colors.blue,
+            icon: Icon(
+              Icons.person,
+              color: Colors.black,
+            ),
+            activeIcon: Icon(
+              Icons.person,
+              color: Colors.blue,
+            ),
+            title: Text('Profile'),
+          ),
+        ],
+      ),
+      body: (_currentIndex == 0)
+          ? homeScreen()
+          : (_currentIndex == 1)
+              ? requestScreen()
+              : ProfileScreenWidget(
+                  user: _currentUser,
+                ),
     );
   }
 }
